563f3bda4133bdd59702c72a024525cd323caf0e
psfauzi <psfauzi@gmail.com>
2020-12-23 18:18:57 +0700

    Belajar Dasar OOP

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ print(p1.hitung_luas())
 s1 = Segitiga(10,3)
 print(s1.info())
 print(s1.hitung_luas())
+
+print('\nMencoba Membuat Objek dari Kelas BangunRuang')
+b1 = BangunRuang()
+print(b1.info())
+print(b1.hitung_luas())
+
+#Molymorpishm : Kemampuan Objek untuk merespon berbeda, terhadap pemanggilan method yang sama
+
+daftar_bangun_ruang = []
+daftar_bangun_ruang.append(p1)
+daftar_bangun_ruang.append(s1)
+
+print('\nPolymorphism')
+for bangun_ruang in daftar_bangun_ruang:
+    print(bangun_ruang.info())
